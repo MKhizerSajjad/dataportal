bd75318c6c4b40ab8f33f41cd48a1dab46426e47
Khizer Sajjad <khizer.sajjad@sixlogics.com>
2024-07-19 22:42:55 +0500

    technologies

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -296,15 +296,15 @@ if ($filters) {
                 });
                 break;
             // case 'technologies':
-            //     $contacts->where(function($query) use ($filter) {
-            //         foreach ((array) $filter as $value) {
-            //             $words = explode(' ', $value);
-            //             foreach ($words as $word) {
-            //                 $query->orWhere('technologies', 'LIKE', '%' . $word . '%');
-            //             }
-            //         }
-            //     });
-            //     break;
+                $contacts->where(function($query) use ($filter) {
+                    foreach ((array) $filter as $value) {
+                        $words = explode(' ', $value);
+                        foreach ($words as $word) {
+                            $query->orWhere('technologies', 'LIKE', '%' . $word . '%');
+                        }
+                    }
+                });
+                break;
         }
     }
 
